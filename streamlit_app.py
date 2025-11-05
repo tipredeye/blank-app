112eec693747bac7c8ec2e9290a218b5ab33d04b
tipredeye <tipredeye@gmail.com>
2025-11-05 09:34:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+import gspread
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#page setup
+st.set_page_config(layout="wide")
+st.title("12 signs of the zodiac")
+st.write(pd.DataFrame({
+    'Sign_ID': ["Aries","Taurus","Gemini","Cancer",
+                "Leo","Virgo","Libra","Scorpio",
+                "Sagittarius","Capricorn","Aquarius","Pisces"],
+    'Elements': ["Fire","Earth","Air","Water",
+                 "Fire","Earth","Air","Water",
+                 "Fire","Earth","Air","Water",
+                 "Fire","Earth","Air","Water"]
+}))
+
+array1= ["Aries","Taurus","Gemini","Cancer",
+         "Leo","Virgo","Libra","Scorpio",
+         "Sagittarius","Capricorn","Aquarius","Pisces"]
+array2= ["Fire","Earth","Air","Water",
+         "Fire","Earth","Air","Water",
+         "Fire","Earth","Air","Water",
+         "Fire","Earth","Air","Water"]
+if len("array1") == len("array2"):
+        df= pd.DataFrame({'Sign_ID':array1,'Elements':array2})
